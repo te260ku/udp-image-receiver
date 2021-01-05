dee959f766ce66f8641cb977aa6fd7836dda22ca
te260ku <teturou@aw.wakwak.com>
2021-01-05 20:14:32 +0900

    バイト列をテクスチャに変換する処理を作成

---
+++ Assets/Scripts/TextureLoaderFromBytes.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class TextureLoaderFromBytes : MonoBehaviour
+{
+    private string rootPath;
+    private float span = 3f;
+    private int currentImageNum;
+    private int maxImageNum = 2;
+    void Start() {
+        rootPath = Application.dataPath + "/Textures/";
+        StartCoroutine(ChangeTexture());
+    }
+
+    IEnumerator ChangeTexture(){
+        while (true) {
+            var texture = ReadTexture("sample-image-" + currentImageNum.ToString() + ".jpeg");
+            GetComponent<Renderer>().material.mainTexture = texture;
+
+            currentImageNum ++;
+            if (currentImageNum > maxImageNum-1) currentImageNum = 0;
+            
+            yield return new WaitForSeconds(span);
+            
+        }
+    }
+
+    Texture ReadTexture(string path)
+    {
+        byte[] readBinary = ReadFile(rootPath + path);
+
+        Texture2D texture = new Texture2D(1, 1);
+        texture.LoadImage(readBinary);
+
+        return texture;
+    }
+
+    byte[] ReadFile(string path)
+    {
+        FileStream fileStream = new FileStream(path, FileMode.Open, FileAccess.Read);
+        BinaryReader bin = new BinaryReader(fileStream);
+        byte[] values = bin.ReadBytes((int)bin.BaseStream.Length);
+
+        bin.Close();
+
+        return values;
+    }
+}
