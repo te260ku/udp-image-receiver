3f708d298b70ef2cc76d7ac67cc264b06e1efe41
te260ku <teturou@aw.wakwak.com>
2021-01-05 19:45:20 +0900

    pythonから送られてくる数値を受信する処理を作成

---
+++ Assets/Scripts/SimpleUDPReceiver.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+public class SimpleUDPReceiver : MonoBehaviour
+{
+    static UdpClient udp;
+    IPEndPoint remoteEP = null;
+    private int i;
+    
+    void Start () {
+        int LOCA_LPORT = 12345;
+
+        udp = new UdpClient(LOCA_LPORT);
+        udp.Client.ReceiveTimeout = 2000;
+    }
+
+    
+    void Update () {
+        IPEndPoint remoteEP = null;
+        byte[] data = udp.Receive(ref remoteEP);
+        string text = Encoding.UTF8.GetString(data);
+        Debug.Log(text);
+    }
+}
