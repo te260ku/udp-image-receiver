847140fc7503a7e907e1727b21c74ff8f8a5ee8c
te260ku <teturou@aw.wakwak.com>
2021-01-05 21:47:42 +0900

    pythonから送信したバイト列を受信して表示できるようにした

--- Assets/Scripts/SimpleUDPReceiver.cs
+++ Assets/Scripts/SimpleUDPReceiver.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Net;
@@ -8,22 +9,31 @@ using System.Threading;
 
 public class SimpleUDPReceiver : MonoBehaviour
 {
+    private int LOCA_LPORT = 12345;
     static UdpClient udp;
-    IPEndPoint remoteEP = null;
-    private int i;
-    
-    void Start () {
-        int LOCA_LPORT = 12345;
+    Thread thread;
 
+    void Start ()
+    {
         udp = new UdpClient(LOCA_LPORT);
-        udp.Client.ReceiveTimeout = 2000;
+        udp.Client.ReceiveTimeout = 0;
+        thread = new Thread(new ThreadStart(ThreadMethod));
+        thread.Start(); 
     }
 
-    
-    void Update () {
-        IPEndPoint remoteEP = null;
-        byte[] data = udp.Receive(ref remoteEP);
-        string text = Encoding.UTF8.GetString(data);
-        Debug.Log(text);
+    void OnApplicationQuit()
+    {
+        thread.Abort();
     }
+
+    private static void ThreadMethod()
+    {
+        while(true)
+        {
+            IPEndPoint remoteEP = null;
+            byte[] data = udp.Receive(ref remoteEP);
+            string text = Encoding.UTF8.GetString(data);
+            Debug.Log(text);
+        }
+    } 
 }
